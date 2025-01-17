2e9c6491291d318e2fa33eea12d681d85e7f0f3b
snowman6-git <chldlfgks55@gmail.com>
2025-01-17 09:11:09 +0900

    no_empty 정규식 $ 실수

--- lib/SGears.ts
+++ lib/SGears.ts
@@ -8,6 +8,6 @@ export function uuid_gen(){
   return uuid
 }
 export function no_empty(...args: string[]): boolean {
-    const regex = /[\s$]*/ // [공백문자나 빈문자가] *존재한다면
+    const regex = /[\s]*/ // [공백문자나 빈문자가] *존재한다면
     return !args.some((arg) => regex.test(arg)); // 문자가 있더라도 공백이 포함되면 false !를 추가하여 있으면 true인것을 뒤집음
 }
